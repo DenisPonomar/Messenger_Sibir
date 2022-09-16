2edff6f56639b81699745cc4b923623e0ce1d5b0
DenisPonomar <104255472+DenisPonomar@users.noreply.github.com>
2022-09-16 19:57:34 +0700

    Add files via upload

---
+++ v 4.0/server/messenger_sibir_v_4.0_server.py
@@ -0,0 +1,84 @@
+import time
+import http.server
+from io import BytesIO
+import hashlib
+import os
+hostName = ""
+serverPort = 15421 #You can choose any available port; by default, it is 8000
+class MyServer(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        pass
+    def do_GET(self): #the do_GET method is inherited from BaseHTTPRequestHandler
+        zapros = self.requestline.split()[1][1:].split("*")
+        print(zapros)
+        try:
+            if zapros[0] == "a":
+                token_otpravitel = zapros[1]
+                hash_token_otpravitel = hashlib.sha256(token_otpravitel.encode("utf-8")).hexdigest()
+                if os.path.isdir(hash_token_otpravitel):
+                    papka = os.listdir(hash_token_otpravitel)
+                    otpravka = False
+                    if len(papka) > 0:
+                        for i in range(len(papka)):
+                            pod_papka = os.listdir(str(hash_token_otpravitel+"/"+papka[i]))
+                            #pod_papka.sort()
+                            if len(pod_papka) == 0:
+                                continue
+                            else:
+                                f = open(str(hash_token_otpravitel+"/"+papka[i]+"/"+pod_papka[0]), 'r')
+                                soob = str(f.read())
+                                f.close()
+                                self.send_response(200)
+                                self.send_header("Content-type", "text/html;charset=utf-8")
+                                self.end_headers()
+                                self.wfile.write((papka[i]+"*"+pod_papka[0]+"*"+soob).encode("utf-8"))
+                                os.remove(str(hash_token_otpravitel+"/"+papka[i]+"/"+pod_papka[0]))
+                                otpravka = True
+                                break
+                        if otpravka == False:
+                            self.send_response(404)
+                            self.send_header("Content-type", "text/html;charset=utf-8")
+                            self.end_headers()
+                    else:
+                        self.send_response(404)
+                        self.send_header("Content-type", "text/html;charset=utf-8")
+                        self.end_headers()
+                else:
+                    self.send_response(404)
+                    self.send_header("Content-type", "text/html;charset=utf-8")
+                    self.end_headers()
+            if zapros[0] == "b" and len(zapros) == 4:
+                server_time = time.time()
+                token_otpravitel = zapros[1]
+                hash_token_otpravitel = hashlib.sha256(token_otpravitel.encode("utf-8")).hexdigest()
+                hash_token_poluchatel = zapros[2]
+                soob_otpravitel = zapros[3]
+                
+                if not os.path.isdir(hash_token_poluchatel):
+                    os.mkdir(hash_token_poluchatel)
+                if not os.path.isdir(str(hash_token_poluchatel+"/"+hash_token_otpravitel)):
+                    os.mkdir(str(hash_token_poluchatel+"/"+hash_token_otpravitel))
+                f = open(str(hash_token_poluchatel+"/"+str(hash_token_otpravitel)+"/"+str(server_time)), 'w')
+                f.write(soob_otpravitel)
+                f.close()
+                
+                #print(self.path[1:])
+                self.send_response(200)
+                self.send_header("Content-type", "text/html;charset=utf-8")
+                self.end_headers()
+                self.wfile.write(str(server_time).encode("utf-8"))
+            else:
+                pass
+        except Exception:
+            self.send_response(500)
+            self.send_header("Content-type", "text/html;charset=utf-8")
+            self.end_headers()
+if __name__ == "__main__":        
+  webServer = http.server.ThreadingHTTPServer((hostName, serverPort), MyServer)
+  print("Server started http://%s:%s" % (hostName, serverPort))  #Server starts
+try:
+          webServer.serve_forever()
+except KeyboardInterrupt:
+	pass
+webServer.server_close()  #Executes when you hit a keyboard interrupt, closing the server
+print("Server stopped")
